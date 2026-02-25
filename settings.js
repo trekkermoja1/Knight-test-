31a8d69ff6a6b787df347a1453e4be58314a8af7
LEE Tech <networkserver06@gmail.com>
2026-02-25 22:29:03 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/networkserver06-commits/LEE TECHbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/networkserver06-commits//Knightbot-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
